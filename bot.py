89038a39107bce77c248b715178ddc70f3e29116
harrie109 <sohiharrie3@gmail.com>
2025-07-29 19:39:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import logging
 from telegram.ext import Updater, CommandHandler
 
-TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+TOKEN = "8472184215:AAG7bZCJ6yprFlGFRtN3kB8IflyuRpHLdv8"
+CHAT_ID = "6234179043"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
